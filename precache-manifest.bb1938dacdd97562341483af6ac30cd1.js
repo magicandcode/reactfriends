a22ed06cbc98641c7130972f24e0007371ee57e4
magicandcode <magicandcode@gmail.com>
2020-04-19 18:39:54 +0200

    Updates

---
+++ precache-manifest.bb1938dacdd97562341483af6ac30cd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "679a2087f7ec8f2c51c5ed99048cd87b",
+    "url": "/reactfriends/index.html"
+  },
+  {
+    "revision": "6d7ad2fc71bdf1214b97",
+    "url": "/reactfriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "97c22baad667aa717289",
+    "url": "/reactfriends/static/css/main.d2511a50.chunk.css"
+  },
+  {
+    "revision": "6d7ad2fc71bdf1214b97",
+    "url": "/reactfriends/static/js/2.5b237668.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/reactfriends/static/js/2.5b237668.chunk.js.LICENSE"
+  },
+  {
+    "revision": "97c22baad667aa717289",
+    "url": "/reactfriends/static/js/main.b01e4693.chunk.js"
+  },
+  {
+    "revision": "bffbfad5519632a2bd8b",
+    "url": "/reactfriends/static/js/runtime-main.f2f1f135.js"
+  }
+]);
